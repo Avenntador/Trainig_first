c3d172fde15ce804d7e9d5c1a1deae3b03d2616f
Avenntador <bulat40k@gmail.com>
2021-05-08 21:58:41 +0300

    Added 4 new components, added redirecting in navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import Navbar from "./Navbar/Navbar";
 import Header from "./Header/Header";
 import Profile from "./Profile/Profile";
 import React from "react";
+import Dialogs from "./Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./News/News";
+import Music from "./Music/Music";
+import Settings from "./Settings/Settings";
 
 const App = () => {
-  return (
-      <div className="wrapper">
-        <Header />
-        <Navbar />
-        <Profile />
-      </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="wrapper-content">
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
+            </div>
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,11 +1,25 @@
 import profileCSS from "./Navbar.module.css"
+import {NavLink} from "react-router-dom";
+import React from "react";
 
 const Navbar = () => {
     return(
         <div className= {profileCSS.nav}>
-            <li>News</li>
-            <li>Messages</li>
-            <li>Music</li>
+            <div className={profileCSS.item}>
+                <NavLink to="/profile" activeClassName={profileCSS.active}>Profile</NavLink>
+            </div>
+            <div className={profileCSS.item}>
+                <NavLink to ="/dialogs" activeClassName={profileCSS.active}>Messages</NavLink>
+            </div>
+            <div className={profileCSS.item}>
+                <NavLink to="/news" activeClassName={profileCSS.active}>News</NavLink>
+            </div>
+            <div className={profileCSS.item}>
+                <NavLink to="/music" activeClassName={profileCSS.active}>Music</NavLink>
+            </div>
+            <div className={profileCSS.item}>
+                <NavLink to='settings' activeClassName={profileCSS.active}>Settings</NavLink>
+            </div>
         </div>
     )
 }
