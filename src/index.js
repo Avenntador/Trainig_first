bcc77fe80cde7265b98f18eb36afb5c2389f0ab2
Avenntador <bulat40k@gmail.com>
2021-06-08 21:58:11 +0300

    pagination for users

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ ReactDOM.render(
 
         <Provider store={store}>
             <App state={store.getState()}/>
-        </Provider>
-   ,
+        </Provider>,
     document.getElementById('root')
 );
 
